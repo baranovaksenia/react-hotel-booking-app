673a09dc41809ccf962378204afd1c2dfba83cf8
Ksenia <baranova-ksenia1990@gmail.com>
2024-04-01 11:39:01 +0300

    setup react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,35 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
 import GlobalStyles from "./styles/GlobalStyles"
-import Button from "./ui/Button"
-import Heading from "./ui/Heading"
-import Row from "./ui/Row"
+
+import Account from "./pages/Account"
+import Bookings from "./pages/Bookings"
+import Cabins from "./pages/Cabins"
+import Dashboard from "./pages/Dashboard"
+import Login from "./pages/Login"
+import PageNotFound from "./pages/PageNotFound"
+import Settings from "./pages/Settings"
+import Users from "./pages/Users"
+import AppLayout from "./ui/AppLayout"
 const App = () => {
 	return (
 		<>
 			<GlobalStyles />
-			<Row type="horizontal">
-				<Heading as="h1">Заголовок h3</Heading>
-				<Heading as="h2">Заголовок h3</Heading>
-				<Heading as="h3">Заголовок h3</Heading>
-			</Row>
+			<BrowserRouter>
+				<Routes>
+					<Route element={<AppLayout />}>
+						<Route index element={<Navigate replace to="dashboard" />} />
+						<Route path="dashboard" element={<Dashboard />} />
+						<Route path="bookings" element={<Bookings />} />
+						<Route path="cabins" element={<Cabins />} />
+						<Route path="users" element={<Users />} />
+						<Route path="settings" element={<Settings />} />
+						<Route path="account" element={<Account />} />
+					</Route>
 
-			<Row type="vertical">
-				<Button size="small" variation="primary">
-					Маленькая кнопка
-				</Button>
-				<Button size="large" variation="secondary">
-					Большая вторичная кнопка
-				</Button>
-				<Button size="medium" variation="danger">
-					Опасная средняя кнопка
-				</Button>
-			</Row>
+					<Route path="login" element={<Login />} />
+					<Route path="*" element={<PageNotFound />} />
+				</Routes>
+			</BrowserRouter>
 		</>
 	)
 }
---
+++ src/pages/Bookings.jsx
@@ -0,0 +1,12 @@
+import Heading from "../ui/Heading"
+import Row from "../ui/Row"
+const Bookings = () => {
+	return (
+		<Row>
+			<Heading as="h1">Booking</Heading>
+			<p>test</p>
+		</Row>
+	)
+}
+
+export default Bookings

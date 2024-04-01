6710cc75de229708c1223ecb61c31e57373f732a
Ksenia <baranova-ksenia1990@gmail.com>
2024-04-01 11:15:52 +0300

    add reusable heading, row, button components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import GlobalStyles from "./styles/GlobalStyles"
+import Button from "./ui/Button"
+import Heading from "./ui/Heading"
+import Row from "./ui/Row"
 const App = () => {
 	return (
 		<>
 			<GlobalStyles />
+			<Row type="horizontal">
+				<Heading as="h1">Заголовок h3</Heading>
+				<Heading as="h2">Заголовок h3</Heading>
+				<Heading as="h3">Заголовок h3</Heading>
+			</Row>
+
+			<Row type="vertical">
+				<Button size="small" variation="primary">
+					Маленькая кнопка
+				</Button>
+				<Button size="large" variation="secondary">
+					Большая вторичная кнопка
+				</Button>
+				<Button size="medium" variation="danger">
+					Опасная средняя кнопка
+				</Button>
+			</Row>
 		</>
 	)
 }
